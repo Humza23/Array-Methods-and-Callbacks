dbd1e0046eed21a41ccf3d80f3191340007dd25c
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-03-23 19:29:58 -0400

    finally done

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import { fifaData } from "./fifa.js";
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-const final2014 = fifaData.filter(function (item) {
+const final2014 = fifaData.filter(function(item) {
   return item.Year === 2014 && item.Stage === "Final";
 });
 //(a) Home Team name for 2014 world cup final
-console.log(final2014[0]["Home Team Name"]);
+console.log('Task 1:', final2014[0]["Home Team Name"]);
 
 //(b) Away Team name for 2014 world cup final
 console.log(final2014[0]["Away Team Name"]);
@@ -35,28 +35,22 @@ function getFinals(data) {
   });
   return finalTeams;
 }
-
+console.log('task 2:', getFinals(fifaData));
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
 1. Receive an array
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(array, getFinals) {
-    // const years = [];
-    // const teamsInFinals = getFinals()
-    // teamsInFinals.forEach(function(item){
-    //     years.push(item.Year)
-    // })
-    // return years
-
-    const teamsInFinals = getFinals()
+function getYears(data, getFinalsCB) {
+    const teamsInFinals = getFinalsCB(data)
     const years = teamsInFinals.map(function (item){
         return item.Year
     })
 return years
-
 }
+
+console.log('task 3:', getYears(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
@@ -65,17 +59,22 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */
 
-function getWinners(array, getFinals) {
-  const winners = [];
-  const teamsInFinals = getFinals()
-  teamsInFinals.forEach(function(item){
-      if(item["Home Team Goals"] > item["Away Team Goals"]){
-          winners.push(item["Home Team Name"])
-      }
-          winners.push(item['Away Team Name'])
-  })
-  return winners
+function getWinners(data, getFinalsCB) {
+    return getFinalsCB(data).map(function(item){
+        return item["Home Team Goals"] > item["Away Team Goals"] ? item["Home Team Name"] :
+        item["Away Team Name"];
+    })
 }
+
+//   const teamsInFinals = getFinals()
+//   teamsInFinals.forEach(function(item){
+//       if(item["Home Team Goals"] > item["Away Team Goals"]){
+//           winners.push(item["Home Team Name"])
+//       }
+//           winners.push(item['Away Team Name'])
+//   })
+//   return winners
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use the higher-order function getWinnersByYear to do the following:
